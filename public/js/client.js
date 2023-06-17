5d505d3e380baf96bce4e41751eb635bf329571b
shashank4628 <shashankgpta7@gmail.com>
2023-06-17 10:34:36 +0530

    Basic App Ready to Deploy

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,82 @@
 const socket = io();
+// you will have have to give room and username here;
+// var username = document.querySelector('#username1').value;
+// var room = document.querySelector('#room1').value;
+var username;
+var room;
+var chats = document.querySelector('.chats');
+var users_list = document.querySelector('.users-list');
+var users_count = document.querySelector('.users-count');
+var msg_send=document.querySelector('#user-send');
+var user_msg=document.querySelector('#user-msg');
+
+
+
+do{
+    username = prompt("Enter your name");
+    room = prompt("Enter room name");
+}while(!username || !room);
+
+
+
+socket.emit('new-user-joined', username);
+
+socket.on('user-connected', (socket_name)=>{
+    // console.log(username + " connected");
+    userJoinLeft(socket_name,'joined');
+});
+
+
+function userJoinLeft(name, status){
+    let div = document.createElement('div');
+    div.classList.add('user-join');
+    let content = `<p><b>${name}</b> ${status} the chat</p>`;
+    div.innerHTML = content;
+    chats.appendChild(div);
+    chats.scrollTop = chats.scrollHeight;
+}
+
+socket.on('user-disconnected', (user)=>{
+    userJoinLeft(user, 'Left');
+});
+// This is for users list
+socket.on('user-list', (users)=>{
+    users_list.innerHTML = '';
+    users_arr=Object.values(users);
+    for(var i=0;i<users_arr.length;i++){
+        let p=document.createElement('p');
+        p.innerText=users_arr[i];
+        users_list.appendChild(p);
+    //
+    }
+    users_count.innerText = users_arr.length;
+});
+
+msg_send.addEventListener('click', ()=>{
+    let data={
+        'username': username,
+        'message': user_msg.value
+    }
+    if(user_msg.value != '') {
+        appendMessage(data, 'outgoing');
+        socket.emit('message', data);
+        user_msg.value = '';
+    }
+})
+
+function appendMessage(data, status){
+    let div=document.createElement('div');
+    div.classList.add('message', status);
+    let content = `
+        <h5>${data.username}</h5>
+        <p>${data.message}</p>
+    `;
+    div.innerHTML = content;
+    chats.appendChild(div);
+    chats.scrollTop = chats.scrollHeight;
+}
+
+
+socket.on('message', (data)=>{
+    appendMessage(data, 'incoming');
+})  
--- server.js
+++ server.js
@@ -14,15 +14,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/login.html');
-})
-
-app.post('/', (req,res)=>{
-    var username = req.body.username;
-    var room = req.body.room;
-    console.log(username+room);
+    // res.sendFile(__dirname + '/login.html');
     res.sendFile(__dirname + '/index.html');
 })
+
+// app.post('/', (req,res)=>{
+//     // username = req.body.username;
+//     // room = req.body.room;
+//     // console.log(username+room);
+//     res.sendFile(__dirname + '/index.html');
+// })
 
 
 
@@ -30,9 +31,33 @@ app.post('/', (req,res)=>{
 /*Socket.io connection*/
 
 const io = require('socket.io')(server);
+var users={};
+
 
 io.on('connection', (socket)=>{
-    console.log(socket.id);
+    // console.log(socket.id);
+    socket.on('new-user-joined', (username)=>{
+        users[socket.id] = username;
+        // socket.broadcast.emit('user-joined', username);
+        // console.log("Hello");
+        // console.log(users);
+        // console.log("Hii");
+        socket.broadcast.emit('user-connected', username);
+        io.emit('user-list', users);
+    })
+
+    socket.on("disconnect", ()=>{
+        // socket.broadcast.emit('user-left', users[socket.id]);
+        socket.broadcast.emit('user-disconnected', user=users[socket.id]);
+        delete users[socket.id];
+        io.emit('user-list', users);
+    })
+
+    socket.on('message', (data)=>{
+        // console.log(data);
+        socket.broadcast.emit('message', {'username': data.username, 'message': data.message});
+        // appendMessage(data, 'incoming');
+    })
 })
 
 
